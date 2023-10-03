c24f1b6c2edff3e1091860c8cde6d79d6474f318
Daniel Cirino <daniel.cirino.02@gmail.com>
2023-10-03 00:23:01 -0300

    Correção do job de geração da análise de tempo de espera.

--- antaq-etl/antaq_etl/job_gerar_analise_tempo_espera.py
+++ antaq-etl/antaq_etl/job_gerar_analise_tempo_espera.py
@@ -12,45 +12,42 @@ spark = client_spark.obterSparkClient(f"analytics")
 
 def salvarAnaliseTempoEspera():
     SQL_ANALISE = """
-        WITH atracacoes_mes as (
-        		select
-        		dt.ano,
-        		dt.mes,
-        		dl.cod_uf ,
-        		dl.regiao_hidrografica,
-        		count(*) qtd_atracacoes, 
-        		sum(af.tempo_espera_atracacao) tempo_espera_atracacao,
-        		sum(af.tempo_atracado) tempo_atracado
-        	from atracacoes_fato  af 
-        		left join dim_tempo dt on CAST(af.dt_atracacao as date) = CAST (dt.[data] as date )
-        		left join dim_localizacao dl on dl.municipio =af.municipio 
-        		left join dim_porto dp on dp.cod_porto_informante = af.cod_porto_informante 
-        	where dt.ano in ('2023','2022','2021')
-        		group by 
-        		dt.ano, dt.mes,
-        		dl.cod_uf ,
-        		dl.regiao_hidrografica
-        )
         SELECT 
-        	cod_uf,
-        	regiao_hidrografica,
-        	ano ano_atual, 
-        	mes mes_atual, 
-        	qtd_atracacoes, 
-        	tempo_espera_atracacao,
-        	tempo_atracado,
-        	 LAG(ano,12) OVER ( ORDER BY ano, mes) AS ano_anterior, 
-        	 LAG(mes,12) OVER ( ORDER BY ano, mes) AS mes_comparacao,
-        	 LAG(qtd_atracacoes,12) OVER ( ORDER BY ano, mes) AS qtd_atracacoes_periodo_anterior,
-        	 qtd_atracacoes - LAG(qtd_atracacoes,12) OVER (ORDER BY ano, mes) AS diferenca,
-        	 (qtd_atracacoes - LAG(qtd_atracacoes,12) OVER (ORDER BY ano, mes)) / CAST(qtd_atracacoes AS DECIMAL) AS variacao_percentual
-        from atracacoes_mes
-        where 1=1 
-        	-- and cod_uf='CE'
-        order by ano,mes  
+            cod_uf,
+            regiao_hidrografica,
+            ano ano_atual, 
+            mes mes_atual, 
+            qtd_atracacoes, 
+            tempo_espera_atracacao,
+            tempo_atracado,
+             LAG(ano,12) OVER ( ORDER BY ano, mes) AS ano_anterior, 
+             LAG(mes,12) OVER ( ORDER BY ano, mes) AS mes_comparacao,
+             LAG(qtd_atracacoes,12) OVER ( ORDER BY ano, mes) AS qtd_atracacoes_periodo_anterior,
+             qtd_atracacoes - LAG(qtd_atracacoes,12) OVER (ORDER BY ano, mes) AS diferenca,
+             (qtd_atracacoes - LAG(qtd_atracacoes,12) OVER (ORDER BY ano, mes)) / CAST(qtd_atracacoes AS DECIMAL) AS variacao_percentual
+        FROM (
+            SELECT
+                dt.ano,
+                dt.mes,
+                dl.cod_uf ,
+                dl.regiao_hidrografica,
+                count(*) qtd_atracacoes, 
+                sum(af.tempo_espera_atracacao) tempo_espera_atracacao,
+                sum(af.tempo_atracado) tempo_atracado
+            FROM atracacoes_fato  af 
+                left join dim_tempo dt on CAST(af.dt_atracacao as date) = CAST (dt.[data] as date )
+                left join dim_localizacao dl on dl.municipio =af.municipio 
+                left join dim_porto dp on dp.cod_porto_informante = af.cod_porto_informante 
+            WHERE dt.ano in ('2023','2022','2021')
+                GROUP BY
+                dt.ano, dt.mes,
+                dl.cod_uf ,
+                dl.regiao_hidrografica) as atracacoes_mes
+        WHERE 1=1 
+            -- and cod_uf='CE' 
     """
-    dfAnalise = client_spark.getDataframeFromSql(client_spark, "antaq", SQL_ANALISE)
-    logging.info(f"Total registros de porto: {dfAnalise.count()}")
+    dfAnalise = client_spark.getDataframeFromSql(spark, "antaq_dw", SQL_ANALISE)
+    logging.info(f"Total registros da analise: {dfAnalise.count()}")
     print("===========================================")
     dfAnalise.show(5)
     print("===========================================")
@@ -62,7 +59,7 @@ def salvarAnaliseTempoEspera():
         path=f"s3a://{BUCKET_ANALYTICS}/{chave}",
         mode="overwrite")
 
-    logging.info(f"Análise tempo de espera salvo no bucket analytics [chave].")
+    logging.info(f"Análise tempo de espera salvo no bucket analytics [{chave}].")
 
 
 parser = argparse.ArgumentParser(prog="Projeto Fiec Antaq - Análise tempo de espera",
